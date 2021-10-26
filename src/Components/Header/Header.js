4d6e549744e2b54c7a028e7583a8af306218df74
sayem254 <sayem254@gmail.com>
2021-10-26 13:02:46 +0200

    added favicon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Navbar className="justify-content-center" bg="dark" variant="light">
+        <Navbar className="justify-content-center g-4" bg="dark" variant="light">
             <Nav className="m-4 p-2">
                 <NavLink to="/home">Home</NavLink>
                 <NavLink to="/service">Services</NavLink>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,7 +11,8 @@ const Home = () => {
         <div >
             <div className='d-flex justify-content-center mb-5'>
                 <Col xs={6} md={7} lg={5}>
-                    <h1> Learn Tech Skills.<span>Build a career you love.</span></h1>
+                    <h1 className='mt-5'>Welcome To CodeEdu</h1> <br />
+                    <h3> Learn Tech Skills.<span>Build a career you love.</span></h3>
                     <p>Immersive Web Development, UX/UI Design, Data Analytics, and Cybersecurity, Youtube Marketing, Graphics Designing, SEO courses.Formats tailored to your needs: remote or on campus, full-time or part-time.we are have more experienced teacher and you can grain your desired skill easily.</p>
                 </Col>
 
